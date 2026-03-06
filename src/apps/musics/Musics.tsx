4158f674b33b42bb67727a3de616812a4bfb8ee7
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2026-03-06 15:27:53 -0300

    feat: volume slider

--- src/apps/musics/Musics.tsx
+++ src/apps/musics/Musics.tsx
@@ -28,6 +28,15 @@ function Musics() {
     setIsPlaying(!isPlaying);
   };
 
+  const changeVolume = (volumeQuantity: number) => {
+    if (!iframeRef.current?.contentWindow) { return; }
+
+    iframeRef
+      .current
+      .contentWindow
+      .postMessage('{"event":"command","func":"setVolume","args":[' + volumeQuantity + ']}', '*');
+  };
+
   return (
     <div className="bg-gray-800 p-2">
       { hasStarted && (
@@ -40,21 +49,35 @@ function Musics() {
         />
       ) }
 
-      <div className="flex items-center justify-center w-64 h-32 overflow-hidden bg-gray-900 rounded-lg mb-2">
+      <div className="flex items-center justify-center w-70 pb-4 h-32 overflow-hidden bg-gray-900 rounded-lg mb-2">
         { isPlaying && (
           <CatAnimation />
         ) }
       </div>
 
-      <div className="flex p-0.5">
+      <div className="flex items-center gap-2 mt-4 mb-2.5 justify-center">
+        <span className="text-sm font-bold text-gray-300">{ isPlaying ? `${t('musics.playing_song')} Lofi Girl Radio` : t('musics.paused_song') }</span>
+      </div>
+      <div className="flex items-center gap-2 justify-center">
         <button
           onClick={ togglePlay }
           className="hover:bg-white/10 rounded-full p-2 cursor-pointer duration-300 transition">
           { isPlaying ? <Pause /> : <Play /> }
         </button>
-        <div className="flex items-center gap-2 ml-5 ">
-          <span className="text-sm font-bold text-gray-300">{ isPlaying ? `${t('musics.playing_song')} Lofi Girl Radio` : t('musics.paused_song') }</span>
-        </div>
+        <span>Vol: </span>
+        <input min={ 0 }
+          max={ 100 }
+          type="range"
+          className="mt-1
+          appearance-none
+          [&::-webkit-slider-runnable-track]:rounded-full
+          [&::-webkit-slider-runnable-track]:bg-gray-900
+          [&::-webkit-slider-thumb]:appearance-none
+          [&::-webkit-slider-thumb]:h-3.75
+          [&::-webkit-slider-thumb]:w-3.75
+          [&::-webkit-slider-thumb]:rounded-full
+          [&::-webkit-slider-thumb]:bg-purple-500"
+          onChange={ (e) => changeVolume(Number(e.target.value)) }/>
       </div>
     </div>
   );
