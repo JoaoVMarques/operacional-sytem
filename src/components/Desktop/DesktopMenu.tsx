4d60102b86528e21e924d9aed088920f23fae064
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2026-03-11 18:48:15 -0300

    feat: delay before menu disapear

--- src/components/Desktop/DesktopMenu.tsx
+++ src/components/Desktop/DesktopMenu.tsx
@@ -2,6 +2,8 @@ import { ChevronRight, Wallpaper, Terminal } from 'lucide-react';
 import { useLanguageStore } from '../../store/useLanguageStore';
 import { useWindowStore } from '../../store/useWindow';
 import { useSettingsStore } from '../../store/useSettings';
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface props {
   menu: {
@@ -15,6 +17,8 @@ function DesktopMenu({ menu }: props) {
   const { t } = useLanguageStore();
   const { openWindow } = useWindowStore();
   const { setTheme } = useSettingsStore();
+
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   return (
     <div
@@ -38,7 +42,11 @@ function DesktopMenu({ menu }: props) {
         <Terminal size={ 16 } className="text-slate-300" />
         <span>{ t('menu.open_terminal') }</span>
       </button>
-      <div className="relative group">
+      <div
+        className="relative group"
+        onMouseEnter={ () => setIsSubMenuOpen(true) }
+        onMouseLeave={ () => setIsSubMenuOpen(false) }
+      >
         <button
           onClick={ (e) => e.stopPropagation() }
           className="w-full
@@ -59,49 +67,55 @@ function DesktopMenu({ menu }: props) {
           </div>
           <ChevronRight size={ 16 } className="text-slate-400" />
         </button>
-        <div className="absolute
-        left-full
-        top-0
-        -ml-1
-        hidden
-        group-hover:flex
-        flex-col
-        w-48
-        bg-slate-800
-        border
-        border-slate-600
-        shadow-2xl
-        rounded-md
-        py-1
-        z-101">
-          <button
-            onClick={ () => setTheme('default') }
-            className="text-left
-            px-4
-            py-1.5
-            text-sm
-            text-gray-200
-            hover:bg-slate-700
-            hover:text-white
-            transition-colors">
-          Default
-          </button>
-          <button
-            onClick={ () => setTheme('darkBlue') }
-            className="text-left
-            px-4
-            py-1.5
-            text-sm
-            text-gray-200
-            hover:bg-slate-700
-            hover:text-white
-            transition-colors">
-          Dark Blue
-          </button>
-        </div>
-
+        <AnimatePresence>
+          { isSubMenuOpen && (
+            <motion.div
+              exit={ {
+                opacity: 1,
+                transition: { delay: 1 },
+              } }
+              className="absolute
+              left-full
+              top-0
+              -ml-1
+              flex
+              flex-col
+              w-48
+              bg-slate-800
+              border
+              border-slate-600
+              shadow-2xl
+              rounded-md
+              py-1
+              z-101"
+            >
+              <button
+                onClick={ () => setTheme('default') }
+                className="text-left
+                px-4
+                py-1.5
+                text-sm
+                text-gray-200
+                hover:bg-slate-700
+                hover:text-white
+                transition-colors">
+                Default
+              </button>
+              <button onClick={ () => setTheme('darkBlue') }
+                className="text-left
+                px-4
+                py-1.5
+                text-sm
+                text-gray-200
+                hover:bg-slate-700
+                hover:text-white
+                transition-colors">
+                Dark Blue
+              </button>
+            </motion.div>
+          ) }
+        </AnimatePresence>
       </div>
-
     </div>
   );
 }
